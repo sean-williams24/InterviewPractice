2e1ddf2385d76fa7144ffb5c16826ed24d5f5fe7
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2020-08-13 12:10:22 +0100

    feat: touch ID

--- Interview Practice/ViewController.swift
+++ Interview Practice/ViewController.swift
@@ -8,6 +8,7 @@
 
 import AVFoundation
 import GameplayKit
+import LocalAuthentication
 import Lottie
 import UIKit
 
@@ -22,6 +23,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .play, target: self, action: #selector(playAudio))
+        
+        authenticateUser()
         
         // Lottie Animations
         
@@ -210,6 +213,36 @@ class ViewController: UIViewController {
     
     func sum(_ nums: Int...) -> Int {
         return nums.reduce(0, +)
+    }
+    
+    
+    // Touch ID
+    
+    func authenticateUser() {
+        let context = LAContext()
+        var error: NSError?
+
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Identify yourself!"
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+                [unowned self] success, authenticationError in
+
+                DispatchQueue.main.async {
+                    if success {
+                        print("Authenticated")
+                    } else {
+                        let ac = UIAlertController(title: "Authentication failed", message: "Sorry!", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        self.present(ac, animated: true)
+                    }
+                }
+            }
+        } else {
+            let ac = UIAlertController(title: "Touch ID not available", message: "Your device is not configured for Touch ID.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
     }
 }
 
